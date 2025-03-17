b8853d4ab1e6a9c9762370820ed9d8254dd9ed93
suren <tadevosyansuren07@gmail.com>
2025-03-17 22:00:26 +0400

    changed about us section

--- src/Components/AboutGYM.tsx
+++ src/Components/AboutGYM.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section className="w-full py-12 bg-gray-900 text-white">
+    <section className="w-full py-12 bg-black text-white">
       <div className="max-w-6xl mx-auto flex flex-col px-6">
         <h2 className="text-4xl md:text-5xl font-bold text-white mb-6 text-start md:text-left">
           ABOUT OUR GYM
         </h2>
 
-        <div className="w-full">
+        <div className="w-full relative">
           <Image
             src="/trainer.jpg"
             alt="Our Gym"
@@ -16,9 +16,15 @@ const About = () => {
             height={400}
             className="rounded-lg shadow-lg w-full object-cover"
           />
+
+
+          <span className="bg-[#D7FB00] px-6  py-3 text-lg text-black rounded-3xl font-bold absolute bottom-4 left-4 md:hidden md:px-8 md:py-4 md:text-xl">
+            10 YEARS
+          </span>
         </div>
 
-        <div className="mt-8 flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
+
+        <div className="mt-12 flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
           <span className="max-w-3xl text-center md:text-left text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non
             nisi nec mi feugiat sagittis vitae nec risus. Sed cursus, lectus sed
@@ -26,7 +32,7 @@ const About = () => {
             turpis sed lacus. Integer nec purus enim.
           </span>
 
-          <span className="bg-[#D7FB00] px-8 py-4 text-xl text-black rounded-3xl inline-flex items-center justify-center text-center">
+          <span className="hidden md:inline-flex bg-[#D7FB00] px-8 py-4 text-center font-bold text-xl text-black rounded-3xl">
             10 YEARS
           </span>
         </div>
